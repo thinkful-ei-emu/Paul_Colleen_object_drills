8c739c063f94bee6e071bdb0804093295dfd18de
Paul Han <kyutae.han93@gmail.com>
2019-06-05 13:59:21 -0400

    loaf assignment called initialize

---
+++ initialize.js
@@ -0,0 +1,8 @@
+'use strict';
+let loaf={
+  flour:300,
+  water:210,
+  //hydration:function(){ return this.water/this.flour*100;},
+  hydration: ()=> loaf.water/loaf.flour*100
+};
+console.log(loaf.hydration());
